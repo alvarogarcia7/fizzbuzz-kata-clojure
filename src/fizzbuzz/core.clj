eef0e18b968b01ea673a42d52cca3a7eeb67b0a6
alvaro garcia <derecerca@gmail.com>
2015-08-16 09:55:21 +0200

    refactor: use extracted function to-num

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -8,4 +8,4 @@
   (cond
     (= n 1) (cons (to-num 1) '())
     (= n 3) (cons (to-num 1) (cons (to-num 2) (cons "fizz" '())))
-    (= n 5) (cons "1" (cons "2" (cons "fizz" (cons "4" (cons "buzz" '())))))))
+    (= n 5) (cons (to-num 1) (cons (to-num 2) (cons "fizz" (cons (to-num 4) (cons "buzz" '())))))))
