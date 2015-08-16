1d38090cba28cfa8783cc87f349c31319cf8c8d8
alvaro garcia <derecerca@gmail.com>
2015-08-16 09:54:25 +0200

    refactor: prefer generating the list than returning a hardcoded list

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -8,4 +8,4 @@
   (cond
     (= n 1) (cons (to-num 1) '())
     (= n 3) (cons "1" (cons "2" (cons "fizz" '())))
-    (= n 5) '("1" "2" "fizz" "4" "buzz")))
+    (= n 5) (cons "1" (cons "2" (cons "fizz" (cons "4" (cons "buzz" '())))))))
