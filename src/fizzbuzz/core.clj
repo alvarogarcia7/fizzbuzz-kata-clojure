28d219fcd7d000d5234ebe4284559829036cafd5
alvaro garcia <derecerca@gmail.com>
2015-08-16 10:43:55 +0200

    refactor: apply last refactor to production code

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -9,4 +9,4 @@
     (cond
       (<= n 2) (map to-num numbers)
       (= n 3) (flatten [(to-num 1) (to-num 2) "fizz"])
-      (= n 5) (cons (to-num 1) (cons (to-num 2) (cons "fizz" (cons (to-num 4) (cons "buzz" '()))))))))
+      (= n 5) (flatten [(to-num 1) (to-num 2) "fizz" (to-num 4) "buzz"]))))
