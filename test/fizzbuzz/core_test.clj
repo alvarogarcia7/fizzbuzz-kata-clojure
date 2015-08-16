afc833b83fdfaa96a8e276f18522eb258b09d1e8
alvaro garcia <derecerca@gmail.com>
2015-08-16 20:33:08 +0200

    refactor: introduce test indirection so it is easy to change

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -3,12 +3,15 @@
   (:require [clojure.string :as str])
   (:require [fizzbuzz.core-with-fixed-schedule :as fs]))
 
+(def fzzbzz
+  fs/fizzbuzz)
+
 (fact "about fizzbuzz"
-  (fs/fizzbuzz 1) => '("1")
-  (fs/fizzbuzz 2) => '("1" "2")
-  (fs/fizzbuzz 3) => '("1" "2" "fizz")
-  (fs/fizzbuzz 5) => '("1" "2" "fizz" "4" "buzz")
-  (fs/fizzbuzz 15) => '("1" "2" "fizz" "4" "buzz" "fizz" "7" "8" "fizz" "buzz" "11" "fizz" "13" "14" "fizzbuzz")
+  (fzzbzz 1) => '("1")
+  (fzzbzz 2) => '("1" "2")
+  (fzzbzz 3) => '("1" "2" "fizz")
+  (fzzbzz 5) => '("1" "2" "fizz" "4" "buzz")
+  (fzzbzz 15) => '("1" "2" "fizz" "4" "buzz" "fizz" "7" "8" "fizz" "buzz" "11" "fizz" "13" "14" "fizzbuzz")
     )
 
 (fact "about number to string converter"
