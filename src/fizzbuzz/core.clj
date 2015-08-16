a217b63bdfd5fd8129b80eda49ad53ea6a433ab8
alvaro garcia <derecerca@gmail.com>
2015-08-16 12:15:04 +0200

    green

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -4,15 +4,22 @@
 (defn to-num[i]
   (str i))
 
+(defn- fizz [n] "fizz")
+(defn- buzz [n] "buzz")
+(defn- fizzbuzz- [n] "fizzbuzz")
+
 (def ^:private fizz-buzz-schedule
-  (flatten (repeat [to-num to-num (fn [n] "fizz") to-num (fn [n] "buzz")])))
+  (flatten (repeat [to-num to-num fizz
+                    to-num buzz fizz
+                    to-num to-num fizz
+                    buzz to-num fizz
+                    to-num to-num fizzbuzz-])))
 
 (defn- zip [functions numbers]
   (map vector functions numbers))
 
-(defn apply-first-on-second [v]
+(defn- apply-first-on-second [v]
   ((first v) (second v)))
-
 
 (defn fizzbuzz[n]
   (let [numbers (range 1 (inc n))
