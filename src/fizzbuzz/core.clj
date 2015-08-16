7e0faf65d6cb5a7ba864d38046f36fd78ac901e1
alvaro garcia <derecerca@gmail.com>
2015-08-16 10:42:52 +0200

    refactor: apply last refactor to production code

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -8,5 +8,5 @@
   (let [numbers (range 1 (inc n))]
     (cond
       (<= n 2) (map to-num numbers)
-      (= n 3) (cons (to-num 1) (cons (to-num 2) (cons "fizz" '())))
+      (= n 3) (flatten [(to-num 1) (to-num 2) "fizz"])
       (= n 5) (cons (to-num 1) (cons (to-num 2) (cons "fizz" (cons (to-num 4) (cons "buzz" '()))))))))
